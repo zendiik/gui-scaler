dbef3c47211fefc6d7c862f0177c904dd9c661f4
Jan Ženíšek <netleak@zendovo.eu>
2026-03-14 18:49:45 +0100

    fix: call resizeDisplay after setting GUI scale

--- neoforge/src/main/java/eu/netleak/guiscaler/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/eu/netleak/guiscaler/platform/NeoForgePlatformHelper.java
@@ -30,6 +30,7 @@ public class NeoForgePlatformHelper implements IPlatformHelper {
     @Override
     public void setGuiScale(int scale) {
         Minecraft.getInstance().options.guiScale().set(scale);
+        Minecraft.getInstance().resizeDisplay();
     }
 
     @Override

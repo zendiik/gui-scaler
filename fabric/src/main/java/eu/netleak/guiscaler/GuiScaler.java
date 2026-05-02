190726375eb19f4d6ed74dadb1ec6595894c62aa
Jan Ženíšek <netleak@zendovo.eu>
2026-05-02 11:29:44 +0200

    refactor(fabric): extract ModMenu integration to separate class

--- fabric/src/main/java/eu/netleak/guiscaler/GuiScaler.java
+++ fabric/src/main/java/eu/netleak/guiscaler/GuiScaler.java
@@ -1,7 +1,5 @@
 package eu.netleak.guiscaler;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
 import eu.netleak.guiscaler.config.GuiScalerConfigModel;
 import eu.netleak.guiscaler.core.ScaleMode;
 import me.shedaniel.autoconfig.AutoConfig;
@@ -11,7 +9,7 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 
 import java.util.Map;
 
-public class GuiScaler implements ModInitializer, ModMenuApi {
+public class GuiScaler implements ModInitializer {
 
     public static GuiScalerConfigModel CONFIG;
 
@@ -45,10 +43,5 @@ public class GuiScaler implements ModInitializer, ModMenuApi {
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             CommonClass.onScreenInit();
         });
-    }
-
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(GuiScalerConfigModel.class, parent).get();
     }
 }
